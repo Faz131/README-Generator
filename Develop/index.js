c2272b80d67f55cbbeab513b276b3d4909b1a335
Faz131 <fazms@icloud.com>
2023-02-25 14:09:40 -0700

    updated JavaScript.  Added questions to prompt the user

--- Develop/index.js
+++ Develop/index.js
@@ -2,28 +2,79 @@
 // File System and Inquirer modules
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = function userInput() {
     return inquirer.prompt([
+
+        //Project title
         {
             type: 'input',
             name: 'name',
             message: 'What is the name of your project?',
         },
 
+        //Description
+        {
+            type: 'input',
+            name: 'description',
+            message: ' Please describe your project.',
+        },
+        //Installation
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What programs or modules were needed to be installed',
+
+        },
+        //usage     
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the intent of this app?',
+        },
+
+        //license goes here
+        {
+            type: 'checkbox',
+            message: 'What license did you use?',
+            name: 'license',
+            choices: ['MIT', 'GPLv2', 'Apache', 'Other'],
+            default: 'MIT',
+
+        },
+
+        //Github username
+        {
+            type: 'input',
+            name: 'GitHubUsername',
+            message: 'Please enter your GitHub username?',
+        },
+
+        //email address
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address',
+        }
     ]);
 };
 //Creates a Markdown File
+
 const createMD = data => {
-    fs.appendFile('test.html', JSON.stringify(data, null, '\t'), (err) =>
+    fs.appendFile('README.md', JSON.stringify(data, null, '\t'), err =>
         err ? console.log(err) : console.log('Success!')
     );
 };
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
-
-// TODO: Create a function to initialize app
-function init() { }
-
-// Function call to initialize app
-init();
+questions()
+    .then(response => {
+        return generateMarkdown(response);
+    })
+    // Displays data from prompts
+    .then(data => {
+        return createMD(data);
+    })
+    // Used to log out any errors that occur
+    .catch(err => {
+        console.log(err)
+    })
