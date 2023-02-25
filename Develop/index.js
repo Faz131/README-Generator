5d297c349aa56564fa0b3d98e3537714abf81f6d
Faz131 <fazms@icloud.com>
2023-02-25 10:42:27 -0700

    Started initial JavaScipt code and installed inquire node_modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,29 @@
-// TODO: Include packages needed for this application
 
+// File System and Inquirer modules
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = function userInput() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+        },
 
+    ]);
+};
+//Creates a Markdown File
+const createMD = data => {
+    fs.appendFile('test.html', JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+};
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
